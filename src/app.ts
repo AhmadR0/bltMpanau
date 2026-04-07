b6e72d489cef59dcd2ca2e250e2ce2ff8cb3f5b9
AhmadR0 <arara21699@gmail.com>
2026-04-07 12:14:07 +0800

    Refactor code structure for improved readability and maintainability

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,19 @@
 import express from "express";
 import type { Express } from "express";
+import cors from "cors"
 import authRoutes from "./routes/authRoutes.js";
 import wargaRoutes from "./routes/wargaRoutes.js";
 
 const app: Express = express();
 
+app.use(cors({
+    origin: "*"
+}));
+
 app.use(express.json());
+
+// Serve folder uploads agar gambar bisa diakses dari Frontend via URL
+app.use("/uploads", express.static("uploads"));
 
 // Routes
 app.use("/api/auth", authRoutes);
